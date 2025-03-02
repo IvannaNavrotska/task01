0ac507ae2684190d71d1555d342117c483c024fa
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-03-02 12:28:02 +0200

    change rle.py

--- rle.py
+++ rle.py
@@ -1,1 +1,12 @@
-print('hello rle')
+example = 'abbbbaaccccc'
+
+def rle_coder(file):
+
+    count = 0
+    example_list = []
+    
+    for bite in file:
+        example_list.append(bite)
+    return example_list
+
+print(rle_coder(example))
