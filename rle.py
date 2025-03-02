72a1a47574734d0fda431486d563201dc0657b27
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-03-02 13:56:03 +0200

    add simple rle decoder function

--- rle.py
+++ rle.py
@@ -11,18 +11,36 @@ def rle_coder(example):
         if example[i] == example[i-1]:
             count +=1
         else:
-            rle_file += f'{count}{example[i-1]}'
+            rle_file += f'{int(count)}{example[i-1]}'
 
             count = 1
             
-    rle_file += f'{count}{example[-1]}'
+    rle_file += f'{int(count)}{example[-1]}'
 
     return rle_file
 
 print(rle_coder(example))
 
 
+def rle_decoder(example):
 
+    file = ''
+    i = 0
+
+    while i < len(example):
+        count = ''
+        while i < len(example) and example[i].isdigit():
+            count += example[i]
+            i += 1
+
+        if i < len(example):
+            file += example[i] * int(count) 
+            i += 1
+
+    return file
+    
+
+print(rle_decoder(rle_coder(example)))
     
 
 
