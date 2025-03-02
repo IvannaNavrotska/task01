e9eb5c6aa14065999c7098b774cd0a83be5375b1
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-03-02 13:04:16 +0200

    add simple rle function

--- rle.py
+++ rle.py
@@ -1,12 +1,23 @@
 example = 'abbbbaaccccc'
 
-def rle_coder(file):
 
-    count = 0
-    example_list = []
+def rle_coder(example):
+
+    rle_file = ''
     
-    for bite in file:
-        example_list.append(bite)
-    return example_list
+    count = 1
+
+    for i in range(1, len(example)):
+        if example[i] == example[i-1]:
+            count +=1
+        else:
+            rle_file += f'{example[i-1]}{count}'
+
+            count = 1
+            
+    rle_file += f'{example[-1]}{count}'
+
+    return rle_file
 
 print(rle_coder(example))
+
